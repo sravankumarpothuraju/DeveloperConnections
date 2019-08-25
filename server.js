f9c749f4f9ff84fa6f5a96126e16c611181bc01c
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2019-08-24 22:16:43 -0400

    Implemented json web token

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const app = express();
 //Connect Database
 connectDB();
 
+//Init Middlewarers
+app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('API running'));
 
 //Define Routes
-app.use('/api/user', require('./routes/api/users'));
+app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
