c887964a2f82b6b5c6646eb2aa7f084dda7917c5
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2019-09-06 14:02:00 -0400

    Added React Components for frontend

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Navbar from './components/layouts/Navbar';
+import Landing from './components/layouts/Landing';
+import './App.css';
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+);
+
+export default App;
