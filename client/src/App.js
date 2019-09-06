4a6cfd94c6ecc3b28bce869e71cead4c652b56f1
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2019-09-06 17:17:23 -0400

    Added Alert Component and Action call

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-import Navbar from './components/layouts/Navbar';
-import Landing from './components/layouts/Landing';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Alert from './components/layout/Alert';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -18,6 +19,7 @@ const App = () => (
 
         <Route exact path='/' component={Landing} />
         <section className='container'>
+          <Alert />
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
